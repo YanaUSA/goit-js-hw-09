4f0bb166d8a9f3e980ed5943baead3cd0ee7a51c
YanaUSA <usatiukiana@gmail.com>
2022-11-03 14:55:05 +0200

    finished 1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtnRef = document.querySelector("button[data-start]");
+const stopBtnRef = document.querySelector("button[data-stop]");
 
+let timerId = null;
+
+startBtnRef.addEventListener("click", () => {
+    startBtnRef.setAttribute("disabled", "");
+
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = `${getRandomHexColor()}`;        
+
+        function getRandomHexColor() {
+            return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+        }        
+    }, 1000);  
+    
+    
+});
+
+stopBtnRef.addEventListener("click", () => {
+    startBtnRef.removeAttribute("disabled"); 
+    document.body.style.backgroundColor = "transparent"; 
+    clearInterval(timerId);    
+});
